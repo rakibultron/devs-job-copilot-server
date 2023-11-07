7dcc732460e1991f72547995099a79b813eb84b2
rakibul-dev <mdrakibul.dev@gmail.com>
2023-11-07 23:24:18 +0600

    databse connection and main puppeteer init func added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,231 @@
 const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 const puppeteer = require("puppeteer-extra");
 const ProxyChain = require("proxy-chain");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 const { autoScroll } = require("./helpers/autoscroll");
-const { scrapjobDetails } = require("./scripts/job_detals_scrapper");
+const { scrapjobDetails, jobs } = require("./scripts/job_detals_scrapper");
 
 puppeteer.use(StealthPlugin());
 puppeteer.use(require("puppeteer-extra-plugin-anonymize-ua")());
 
-const techs = ["react.js"];
-const countries = ["Afghanistan", "Albania", "Algeria"];
+const techs = ["react.js", "node.js", "mern stack"];
+const countries = [
+  "Afghanistan",
+  "Albania",
+  "Algeria",
+  "Andorra",
+  "Angola",
+  "Antigua and Barbuda",
+  "Argentina",
+  "Armenia",
+  "Australia",
+  "Austria",
+  "Azerbaijan",
+  "Bahamas",
+  "Bahrain",
+  "Bangladesh",
+  "Barbados",
+  "Belarus",
+  "Belgium",
+  "Belize",
+  "Benin",
+  "Bhutan",
+  "Bolivia",
+  "Bosnia and Herzegovina",
+  "Botswana",
+  "Brazil",
+  "Brunei",
+  "Bulgaria",
+  "Burkina Faso",
+  "Burundi",
+  "Côte d'Ivoire",
+  "Cabo Verde",
+  "Cambodia",
+  "Cameroon",
+  "Canada",
+  "Central African Republic",
+  "Chad",
+  "Chile",
+  "China",
+  "Colombia",
+  "Comoros",
+  "Congo (Congo-Brazzaville)",
+  "Costa Rica",
+  "Croatia",
+  "Cuba",
+  "Cyprus",
+  "Czechia (Czech Republic)",
+  "Democratic Republic of the Congo (Congo-Kinshasa)",
+  "Denmark",
+  "Djibouti",
+  "Dominica",
+  "Dominican Republic",
+  "Ecuador",
+  "Egypt",
+  "El Salvador",
+  "Equatorial Guinea",
+  "Eritrea",
+  "Estonia",
+  "Eswatini",
+  "Ethiopia",
+  "Fiji",
+  "Finland",
+  "France",
+  "Gabon",
+  "Gambia",
+  "Georgia",
+  "Germany",
+  "Ghana",
+  "Greece",
+  "Grenada",
+  "Guatemala",
+  "Guinea",
+  "Guinea-Bissau",
+  "Guyana",
+  "Haiti",
+  "Holy See",
+  "Honduras",
+  "Hungary",
+  "Iceland",
+  "India",
+  "Indonesia",
+  "Iran",
+  "Iraq",
+  "Ireland",
+  "Israel",
+  "Italy",
+  "Jamaica",
+  "Japan",
+  "Jordan",
+  "Kazakhstan",
+  "Kenya",
+  "Kiribati",
+  "Kuwait",
+  "Kyrgyzstan",
+  "Laos",
+  "Latvia",
+  "Lebanon",
+  "Lesotho",
+  "Liberia",
+  "Libya",
+  "Liechtenstein",
+  "Lithuania",
+  "Luxembourg",
+  "Madagascar",
+  "Malawi",
+  "Malaysia",
+  "Maldives",
+  "Mali",
+  "Malta",
+  "Marshall Islands",
+  "Mauritania",
+  "Mauritius",
+  "Mexico",
+  "Micronesia",
+  "Moldova",
+  "Monaco",
+  "Mongolia",
+  "Montenegro",
+  "Morocco",
+  "Mozambique",
+  "Myanmar (formerly Burma)",
+  "Namibia",
+  "Nauru",
+  "Nepal",
+  "Netherlands",
+  "New Zealand",
+  "Nicaragua",
+  "Niger",
+  "Nigeria",
+  "North Korea",
+  "North Macedonia (formerly Macedonia)",
+  "Norway",
+  "Oman",
+  "Pakistan",
+  "Palau",
+  "Palestine State",
+  "Panama",
+  "Papua New Guinea",
+  "Paraguay",
+  "Peru",
+  "Philippines",
+  "Poland",
+  "Portugal",
+  "Qatar",
+  "Romania",
+  "Russia",
+  "Rwanda",
+  "Saint Kitts and Nevis",
+  "Saint Lucia",
+  "Saint Vincent and the Grenadines",
+  "Samoa",
+  "San Marino",
+  "Sao Tome and Principe",
+  "Saudi Arabia",
+  "Senegal",
+  "Serbia",
+  "Seychelles",
+  "Sierra Leone",
+  "Singapore",
+  "Slovakia",
+  "Slovenia",
+  "Solomon Islands",
+  "Somalia",
+  "South Africa",
+  "South Korea",
+  "South Sudan",
+  "Spain",
+  "Sri Lanka",
+  "Sudan",
+  "Suriname",
+  "Sweden",
+  "Switzerland",
+  "Syria",
+  "Tajikistan",
+  "Tanzania",
+  "Thailand",
+  "Timor-Leste",
+  "Togo",
+  "Tonga",
+  "Trinidad and Tobago",
+  "Tunisia",
+  "Turkey",
+  "Turkmenistan",
+  "Tuvalu",
+  "Uganda",
+  "Ukraine",
+  "United Arab Emirates",
+  "United Kingdom",
+  "United States of America",
+  "Uruguay",
+  "Uzbekistan",
+  "Vanuatu",
+  "Venezuela",
+  "Vietnam",
+  "Yemen",
+  "Zambia",
+  "Zimbabwe",
+];
+
+console.log("redis check ====>", process.env.REDIS_DB_NAME);
+// Connect to MongoDB
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@my-cluster.wlkgtiv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    {
+      //   useNewUrlParser: true,
+      //   useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log(`app connected with ${process.env.DB_NAME} database 🚀`);
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error: ", err);
+  });
 
 const run = async (techIndex, countryIndex) => {
   function generateRandomNumber() {
@@ -33,7 +249,8 @@ const run = async (techIndex, countryIndex) => {
   const server = new ProxyChain.Server({ port: randomValue });
   await server.listen();
   if (techIndex >= techs.length) {
-    console.log("Scraping completed.");
+    console.log("Scraping completed.", jobs.length);
+
     return;
   }
 
@@ -103,7 +320,7 @@ const run = async (techIndex, countryIndex) => {
       "[data-tracking-control-name='guest_homepage-basic_guest_nav_menu_jobs']"
     );
     await page.waitForSelector("#job-search-bar-keywords");
-    await page.type("#job-search-bar-keywords", tech, { delay: 100 });
+    await page.type("#job-search-bar-keywords", tech, { delay: 50 });
     await page.waitForSelector("#job-search-bar-location");
     await page.click("#job-search-bar-location");
     await page.waitForSelector(
@@ -113,12 +330,12 @@ const run = async (techIndex, countryIndex) => {
     await page.click(
       "section.typeahead-input:nth-child(2) > button:nth-child(3) > icon:nth-child(2)"
     );
-    await page.type("#job-search-bar-location", country, { delay: 100 });
+    await page.type("#job-search-bar-location", country, { delay: 50 });
     await page.waitForSelector(
       "button.base-search-bar__submit-btn:nth-child(5)"
     );
     await page.click("button.base-search-bar__submit-btn:nth-child(5)");
-    await page.waitForTimeout(3000);
+    await page.waitForTimeout(2000);
     await autoScroll(page);
 
     const jobElements = await page.$$(".jobs-search__results-list > li");
@@ -130,7 +347,7 @@ const run = async (techIndex, countryIndex) => {
 
     for (const element of jobElements) {
       await element.click();
-      await page.waitForTimeout(3000);
+      await page.waitForTimeout(1500);
       await scrapjobDetails(page);
     }
 
@@ -143,7 +360,7 @@ const run = async (techIndex, countryIndex) => {
     //   console.log(links);
 
     // await scrapjobDetails(page, links, browser);
-    await page.waitForTimeout(2000);
+    await page.waitForTimeout(500);
     await page.close();
     await browser.close();
   } catch (error) {
