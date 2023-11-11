0ebfb10d1c51a0c61b61fc3122eb5ae36b90066e
rakibul-dev <mdrakibul.dev@gmail.com>
2023-11-11 18:18:33 +0600

    date time func added

--- src/helpers/date_time.js
+++ src/helpers/date_time.js
@@ -1,13 +1,15 @@
-const { sub } = require("date-fns");
+const { sub, toDate, format, parseISO } = require("date-fns");
 
 const postedAt = "4 hours ago";
 const currentTime = new Date();
 
 // Parse and calculate the new date
 const newDate = (postedAt) => {
-  const date = sub(currentTime, { hours: parseInt(postedAt) });
-
+  const timeStamps = sub(currentTime, { hours: parseInt(postedAt) });
+  const date = toDate(timeStamps);
+  console.log(format(date, "hh:mm:ss a dd/LL/yyyy "));
   return date;
 };
 
 newDate(postedAt);
+module.exports = newDate();
