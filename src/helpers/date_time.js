d9644711449f0445a74bd3a36bf9d1d4d5c2c748
rakibul-dev <mdrakibul.dev@gmail.com>
2023-11-23 21:17:28 +0600

    helper func for date and time

--- src/helpers/date_time.js
+++ src/helpers/date_time.js
@@ -1,4 +1,4 @@
-const { sub, toDate, format, parseISO } = require("date-fns");
+const { sub, add, toDate, format, parseISO } = require("date-fns");
 
 const postedAt = "4 hours ago";
 const currentTime = new Date();
@@ -13,9 +13,40 @@ const newDate = (postedAt) => {
 const StringToDate = (postedAt) => {
   const timeStamps = sub(currentTime, { hours: parseInt(postedAt) });
   const date = toDate(timeStamps);
-  //   console.log(format(date, "hh:mm:ss a dd/LL/yyyy"));
-  return date;
+  //   console.log(format(date, "yyyy/LL/dd"));
+  const formatedDate = format(date, "yyyy-MM-dd");
+  return formatedDate;
 };
+
+function calculateRelativeDate(unit, amount) {
+  const currentDate = new Date();
+
+  switch (unit.toLowerCase()) {
+    // case "years":
+    //   return add(currentDate, { years: -amount });
+    // case "months":
+    //   return add(currentDate, { months: -amount });
+    // case "days":
+    //   return add(currentDate, { days: -amount });
+    case "weeks":
+      return format(add(currentDate, { weeks: -amount }), "dd-LL-yyyy");
+    default:
+      throw new Error(
+        "Invalid unit. Supported units are year, month, day, and week."
+      );
+  }
+}
+
+// Example usage:
+// const twoMonthsAgo = calculateRelativeDate("months", 2);
+// const oneYearAgo = calculateRelativeDate("years", 1);
+// const tenDaysAgo = calculateRelativeDate("days", 10);
+const threeWeeksAgo = calculateRelativeDate("weeks", 3);
+
+// console.log("Two months ago:", twoMonthsAgo);
+// console.log("One year ago:", oneYearAgo);
+// console.log("Ten days ago:", tenDaysAgo);
+console.log("Three weeks ago:", threeWeeksAgo);
 
 // newDate(postedAt);
 module.exports = { StringToDate };
